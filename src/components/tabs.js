8710e0e70529241e978a0d197056e6e682ff5958
Johnny Murillo <jossters@gmail.com>
2021-03-12 02:59:40 -0800

    done with tabs

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,3 +1,5 @@
+import axios from "axios"
+
 const Tabs = (topics) => {
   // TASK 3
   // ---------------------
@@ -13,6 +15,33 @@ const Tabs = (topics) => {
   //   <div class="tab">technology</div>
   // </div>
   //
+    const topics1 = document.createElement("div")
+    const tab1 = document.createElement("div")
+    const tab2 = document.createElement("div")
+    const tab3 = document.createElement("div")
+    const tab4 = document.createElement("div")
+    const tab5 = document.createElement("div")
+
+    topics1.appendChild(tab1);
+    topics1.appendChild(tab2);
+    topics1.appendChild(tab3);
+    topics1.appendChild(tab4);
+    topics1.appendChild(tab5);
+
+    topics1.classList.add("topics");
+    tab1.classList.add("tab");
+    tab2.classList.add("tab");
+    tab3.classList.add("tab");
+    tab4.classList.add("tab");
+    tab5.classList.add("tab");
+
+    tab1.textContent = `${topics.data.topics[0]}`;
+    tab2.textContent = `${topics.data.topics[1]}`;
+    tab3.textContent = `${topics.data.topics[2]}`;
+    tab4.textContent = `${topics.data.topics[3]}`;
+    tab5.textContent = `${topics.data.topics[4]}`;
+    
+    return topics1
 }
 
 const tabsAppender = (selector) => {
@@ -23,6 +52,12 @@ const tabsAppender = (selector) => {
   // Find the array of topics inside the response, and create the tabs using the Tabs component.
   // Append the tabs to the element in the DOM that matches the selector passed to the function.
   //
+  axios
+  .get(`https://lambda-times-api.herokuapp.com/topics`)
+  .then(res =>{
+    const tabs = Tabs(res);
+    document.querySelector(selector).append(tabs)
+  })
 }
 
 export { Tabs, tabsAppender }
